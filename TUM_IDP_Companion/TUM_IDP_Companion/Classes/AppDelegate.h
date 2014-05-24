bed082d991bffd989838f7e659e0bf67a811a4b6
Kashan Khan <kashankhaan@gmail.com>
2014-05-24 19:29:08 +0200

    Project Setup

---
+++ TUM_IDP_Companion/TUM_IDP_Companion/Classes/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TUM_IDP_Companion
+//
+//  Created by Kashan Khan on 24/05/2014.
+//  Copyright (c) 2014 Kashan Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TUM_IDP_Companion/TUM_IDP_Companion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TUM_IDP_Companion
+//
+//  Created by Kashan Khan on 24/05/2014.
+//  Copyright (c) 2014 Kashan Khan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
